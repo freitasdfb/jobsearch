76c71f9e722909fc3075c185f20be73cc4d69fde
Victor Barbosa <freitasdfb@gmail.com>
2021-03-12 15:31:12 -0300

    :bug: Bug on import class

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { ContainerGlobal } from '../infra/components/Container'
-import { HomeMain } from './home/index'
+import  HomeMain  from './home/index'
 
 export default function Home() {
   return (
